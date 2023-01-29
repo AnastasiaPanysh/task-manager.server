cfc311e0fcaf02c1620072530905a95ac13aa156
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-01-29 14:04:46 +0300

    add repository.test

--- src/tests/repository/user.reporitory.test.ts
+++ src/tests/repository/user.reporitory.test.ts
@@ -1,4 +1,4 @@
-import { getUsersDB, getUserByIdDB, updateUsersDB, deleteUserDB, patchUsersDB } from '../../repository/user.repository' 
+import { getUsersDB, getUserByIdDB, updateUsersDB, deleteUserDB, patchUsersDB } from '../../repository/user.repository'
 
 const mClient = {
     query: jest.fn()
@@ -11,8 +11,8 @@ jest.mock('pg', () => {
     return { Pool: jest.fn(() => mPool) }
 })
 
-describe('getUsersDB', ()=>{
-    test('should return success', async()=>{
+describe('getUsersDB function', () => {
+    test('should return success', async () => {
         const mockUsers = [{ id: 1, name: 'nasy', surname: 'nasy', pwd: 'qwert', email: 'nasy@gmail.com', status: 1 }]
 
         mClient.query.mockResolvedValue({ rows: mockUsers })
