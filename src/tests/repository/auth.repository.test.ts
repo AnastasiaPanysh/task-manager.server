9ed1c5dd33c151f6628e5c6fce19522624d60c03
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-01-29 14:28:01 +0300

    add repository.tests

---
+++ src/tests/repository/auth.repository.test.ts
@@ -0,0 +1,36 @@
+import { getUserByEmailDB, createUserDB } from '../../repository/auth.repository'
+
+const mClient = {
+    query: jest.fn()
+}
+
+jest.mock('pg', () => {
+    const mPool = {
+        connect: jest.fn(() => mClient)
+    }
+    return { Pool: jest.fn(() => mPool) }
+})
+
+describe('getUserByEmailDB function', () => {
+    test('should return success', async () => {
+        const mockUsers = [{ id: 1, name: 'nasy', surname: 'nasy', pwd: 'qwert', email: 'nasy@gmail.com', status: 1 }]
+
+        mClient.query.mockResolvedValue({ rows: mockUsers })
+        const expected = await getUserByEmailDB('nasy@gmail.com')
+
+        expect(expected).toEqual(mockUsers)
+    })
+})
+
+//does not work
+describe('createUserDB function', () => {
+    test('should return success', async () => {
+        const mockUsers = [{ id: 1, name: 'nasy', surname: 'nasy', pwd: 'qwert', email: 'nasy@gmail.com', status: 1 }]
+
+        mClient.query.mockResolvedValue({ rows: mockUsers })
+        const expected = await createUserDB('nasy','nasy','nasy@gmail.com','qwert')
+
+        expect(mClient.query).toBeCalledWith('COMMIT')
+        expect(expected).toEqual(mockUsers)
+    })
+})
